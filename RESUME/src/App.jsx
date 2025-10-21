8b28804a6184656d974140e9f17d0a16fb01c28e
yashyachwad <yashyechwad420@gmail.com>
2025-10-22 03:29:56 +0530

    App.jsx / LandingPage imported correctly

--- RESUME/src/App.jsx
+++ RESUME/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Route , Routes } from 'react-router-dom'
-import LandingPage from './pages/landingPage'
+import LandingPage from './pages/LandingPage'
 import Dashboard from './pages/Dashboard'
 import EditResume from './components/EditResume'
 import { Toaster } from 'react-hot-toast'
