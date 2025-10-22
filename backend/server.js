06e4d6875f1b858109a11f0ac5ee04bddf08ef8f
yashyachwad <yashyechwad420@gmail.com>
2025-10-22 10:30:31 +0530

    Refactor server.js for CORS and DB connection
    Updated CORS configuration and ensured database connection is established at the start.

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-dotenv.config();
 const connectDB = require('./config/db');
 const userRouter = require('./routes/userRoutes')
 const path = require('path');
 const resumeRoutes = require('./routes/resumeRoutes');
 
 
+dotenv.config();
+// connect  DB
+ connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+
+// ✅ Proper CORS configuration
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://rex-frontend-pf7l.onrender.com'
+];
+
+app.use(cors({
+  origin: 'https://rex-frontend-pf7l.onrender.com',  // your deployed frontend
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true
+}));
+
+
 // Middleware
 app.use(express.json());
-app.use(cors());
-app.use('/api', userRouter);
-app.use('/api/resume', resumeRoutes);
+//app.use(cors());  app.use(cors());
+
 
 app.use('/uploads',
     express.static(path.join(__dirname, 'uploads'), {
@@ -25,15 +41,9 @@ res.set('Access-Control-Allow-Origin', 'http://localhost:5173')
     })
 )
 
+app.use('/api', userRouter);
+app.use('/api/resume', resumeRoutes);
 
-
-app.use(cors({
-  origin: 'https://rex-dwpg.onrender.com/',  // your deployed frontend
-  methods: ['GET','POST','PUT','DELETE'],
-  credentials: true
-}));
-// connect  DB
- connectDB();
 
 
 
